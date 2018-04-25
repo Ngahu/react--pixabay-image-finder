b5ed0acbb2fbbf3f9ff9cfde63c60da6abec2b86
Ngahu <jamaalaraheem@gmail.com>
2018-04-25 13:19:59 +0300

    creatingof the components folder structur and also creating of  both the nav and also the seach components

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import AppBar from 'material-ui/AppBar';
+
+// export const Navbar= () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+
+
+
+export const Navbar = () => <AppBar title="Pixabay Image Finder"/>;
